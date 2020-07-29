a1ee12116b68f4a9971f405ff031cb83deac2981
Anthony Lepore <violacellonyc@gmail.com>
2020-07-29 20:20:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  if array.length == 0 
+   
+  else 
+    i = 0 
+    collection = []
+    while i < array.length do 
+      collection.push(yield(array[i]))
+      i += 1 
+    end
+  end
+  collection
+  
+end
